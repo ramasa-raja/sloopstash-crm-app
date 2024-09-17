e713929e917d3948280ec8286d9ea52b1c99138a
Ramasamy raja <ramasamyraja194@gmail.com>
2024-09-17 20:20:40 +0530

    Requesting  squash merge from my branch to prestaging branch (#1)
    * Implement controller for lead feature
    
    * Add DB model connectors for lead feature
    
    * Include lead feature view templates
    
    * Update lead feature with new functions
    
    * Add DB Connectors for lead feature

---
+++ controller/lead/--init--.py
@@ -0,0 +1,16 @@
+# Lead feature implementation.
+
+class lead():
+
+   def __init__():
+     print 'Lead class constructor.'
+
+   def create():
+     print 'Lead created.'
+
+   def update():
+     print 'Lead update.'
+
+   def view ():
+     print 'View lead.'
+
---
+++ controller/lead/__init__.py
@@ -0,0 +1,18 @@
+# Lead feature implementation.
+ 
+class lead():
+ 
+  def __init__():
+    print 'Lead class constructor.'
+ 
+  def create():
+    print 'Lead created.'
+ 
+  def edit():
+    print 'Lead edited.'
+ 
+  def view():
+    print 'View lead.'
+ 
+  def delete():
+    print 'Delete lead.'
